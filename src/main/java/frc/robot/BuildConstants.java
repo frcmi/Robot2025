a46fa98c85f178ae1e396b88bc9fb6d63a3fd4e0
jbrastad <--global>
2025-04-24 16:08:59 -0700

    updated some things

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 79;
-  public static final String GIT_SHA = "46f26f739712842fa74c710a48194e64ba332792";
-  public static final String GIT_DATE = "2025-02-20 17:55:42 PST";
+  public static final int GIT_REVISION = 82;
+  public static final String GIT_SHA = "58bc53c0106cc610a29187e4a40bc42db2f02b79";
+  public static final String GIT_DATE = "2025-02-22 13:22:29 PST";
   public static final String GIT_BRANCH = "advantage-kit";
-  public static final String BUILD_DATE = "2025-02-20 18:40:08 PST";
-  public static final long BUILD_UNIX_TIME = 1740105608086L;
+  public static final String BUILD_DATE = "2025-04-24 15:54:57 PDT";
+  public static final long BUILD_UNIX_TIME = 1745535297641L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
